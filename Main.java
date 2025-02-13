7ad02643524b852e5785a5f754bcfd6af2faf3b3
arikakira <samanthal242@nycstudents.net>
2025-02-13 16:49:10 +0000

    Update

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        Sign sign1 = new Sign("ABC222DE", 3);
+        x = sign1.numberOfLines();
+        System.out.println(x);
+    }
+}
--- Sign.java
+++ Sign.java
@@ -1,1 +1,23 @@
+public class Sign {
+    private String message;
+    private int width;
 
+    public Sign(String m, int w) {
+        message = m;
+        width = w;
+    }
+    
+    public int numberOfLines() {
+        return message.length()/width + 1;
+    }
+
+    public String getLines() {
+        String s = "";
+        int i = 0;
+        while(i<message.length()) {
+            s += message.substring(i, i+1);
+            i++;
+            if()
+        }
+    }
+}
